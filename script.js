49a49088b69b99c9dcf371b625e6d7b960eac039
sushma-regidi <regidisushma@gmail.com>
2025-09-21 22:08:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  if (arr.length < 2) return -Infinity; // less than 2 elements
+
+  let max = -Infinity;
+  let secondMax = -Infinity;
+
+  for (let num of arr) {
+    if (num > max) {
+      secondMax = max;
+      max = num;
+    } else if (num > secondMax && num < max) {
+      secondMax = num;
+    }
+  }
+
+  return secondMax === -Infinity ? -Infinity : secondMax;
+}
+
+// Examples
+console.log(secondHighest([5, 1, 2, 3, 4]));    // 4
+console.log(secondHighest([-1, -2, -3, -4]));   // -2
+console.log(secondHighest([]));                 // -Infinity
+console.log(secondHighest([1]));                // -Infinity
+console.log(secondHighest([1, 1, 1, 1]));       // -Infinity
